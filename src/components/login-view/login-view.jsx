b0d0b2dd09a0d9f3e686c19adf51d9e63ed42e61
Droidca <david-agostinho@hotmail.com>
2022-03-15 19:29:51 +0000

    Add login and register view

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+export function LoginView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    props.onLoggedIn(username);
+  };
+
+  return (
+    <div className="Forms">
+      <form>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+        <button type="submit" onClick={handleSubmit}>Login</button>
+      </form>
+
+      <div className="signup_button">
+        <a href="...">Resgister Here!</a>
+      </div>
+
+    </div>
+  );
+}
---
+++ src/components/register-view/register-view.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+export function RegisterView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password, email);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    props.onRegistration(username);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Register</button>
+    </form>
+  );
+}
