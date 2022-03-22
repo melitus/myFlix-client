26a23027277e7b5ce8c8e6872a2e42b9216d1d07
Droidca <david-agostinho@hotmail.com>
2022-03-22 23:14:08 +0000

    Apply axios and authentication logic

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 import './login-view.scss';
 import { FormControl } from 'react-bootstrap';
@@ -11,24 +12,38 @@ export function LoginView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
     // Send a request to the server for authentication
-    props.onLoggedIn(username);
+    axios.post('https://popcorns-and-coke.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+      .then(response => {
+        const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log('no such user')
+      });
   };
 
   return (
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label id="label" >Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
+    <div className="login-view">
+      <Form>
+        <Form.Group controlId="formUsername">
+          <Form.Label id="label" >Username:</Form.Label>
+          <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+        </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label id="label" >Password:</Form.Label>
-        <Form.Control type="text" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label id="label" >Password:</Form.Label>
+          <Form.Control type="text" onChange={e => setPassword(e.target.value)} />
+        </Form.Group>
 
-      <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
-    </Form>
+        <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+      </Form>
+
+      <a href="../register" target="_blank">Register Here</a>
+    </div>
+
   );
 }
