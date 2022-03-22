8e58320f81afbb071b84c83ff1802ab0e7108ddb
Droidca <david-agostinho@hotmail.com>
2022-03-22 23:15:02 +0000

    Add axios and register logic

--- src/components/register-view/register-view.jsx
+++ src/components/register-view/register-view.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 import './register-view.scss';
 
@@ -6,30 +9,55 @@ export function RegisterView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+  const [birthday, setBirthday] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password, email);
     /* Send a request to the server for authentication */
+    axios.post('https://popcorns-and-coke.herokuapp.com/users', {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    })
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+        window.open('/', '_self');
+      })
+      .catch(e => {
+        console.log('error registering the user')
+      });
     /* then call props.onLoggedIn(username) */
     props.onRegistration(username);
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Register</button>
-    </form>
+    <Form>
+
+      <Form.Group controlId="formUsername">
+        <Form.Label id="label" >Username:</Form.Label>
+        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label id="label" >Password:</Form.Label>
+        <Form.Control type="text" onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="formEmail">
+        <Form.Label id="label" >Email:</Form.Label>
+        <Form.Control type="email" onChange={e => setEmail(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="formBirthday">
+        <Form.Label id="label" >Birthday:</Form.Label>
+        <Form.Control type="date" onChange={e => setBirthday(e.target.value)} />
+      </Form.Group>
+
+      <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+
+    </Form>
   );
 }
