3e3bf7cde17db1d7798b28a9a7acb2d587d4f6a5
Droidca <david-agostinho@hotmail.com>
2022-03-24 04:16:13 +0000

    Add security in register view

--- src/components/register-view/register-view.jsx
+++ src/components/register-view/register-view.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import Form from 'react-bootstrap/Form';
+import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 
 import './register-view.scss';
@@ -11,26 +12,58 @@ export function RegisterView(props) {
   const [email, setEmail] = useState('');
   const [birthday, setBirthday] = useState('');
 
+  const [usernameErr, setUsernameErr] = useState('');
+  const [passwordErr, setPasswordErr] = useState('');
+  const [emailErr, setEmailErr] = useState('');
+
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr('Username Required');
+      isReq = false;
+    } else if (username.length < 5) {
+      setUsernameErr('Username must be 5 characters long');
+      isReq = false;
+    }
+    if (!password) {
+      setPasswordErr('Password Required');
+      isReq = false;
+    } else if (password.length < 6) {
+      setPasswordErr('Password must be 6 characters long');
+      isReq = false;
+    }
+    if (!email) {
+      setEmailErr('Email required');
+      isReq = false;
+    } else if (email.indexOf('@') === -1) {
+      setEmail('Email must be valid');
+      isReq = false;
+    }
+
+    return isReq;
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password, email);
-    /* Send a request to the server for authentication */
-    axios.post('https://popcorns-and-coke.herokuapp.com/users', {
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthday: birthday
-    })
-      .then(response => {
-        const data = response.data;
-        console.log(data);
-        window.open('/', '_self');
+    const isReq = validate();
+    if (isReq) {
+      /* Send a request to the server for authentication */
+      axios.post('https://popcorns-and-coke.herokuapp.com/users', {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday
       })
-      .catch(e => {
-        console.log('error registering the user')
-      });
-    /* then call props.onLoggedIn(username) */
-    props.onRegistration(username);
+        .then(response => {
+          const data = response.data;
+          console.log(data);
+          alert('Registration successfull!')
+          window.open('/', '_self');
+        })
+        .catch(e => {
+          console.log('error registering the user')
+        });
+    }
   };
 
   return (
@@ -38,22 +71,25 @@ export function RegisterView(props) {
 
       <Form.Group controlId="formUsername">
         <Form.Label id="label" >Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+        <Form.Control type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+        {usernameErr && <p>{usernameErr}</p>}
       </Form.Group>
 
       <Form.Group controlId="formPassword">
         <Form.Label id="label" >Password:</Form.Label>
-        <Form.Control type="text" onChange={e => setPassword(e.target.value)} />
+        <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+        {passwordErr && <p>{passwordErr}</p>}
       </Form.Group>
 
       <Form.Group controlId="formEmail">
         <Form.Label id="label" >Email:</Form.Label>
-        <Form.Control type="email" onChange={e => setEmail(e.target.value)} />
+        <Form.Control type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+        {emailErr && <p>{emailErr}</p>}
       </Form.Group>
 
       <Form.Group controlId="formBirthday">
         <Form.Label id="label" >Birthday:</Form.Label>
-        <Form.Control type="date" onChange={e => setBirthday(e.target.value)} />
+        <Form.Control type="full-date" onChange={e => setBirthday(e.target.value)} />
       </Form.Group>
 
       <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
@@ -61,3 +97,12 @@ export function RegisterView(props) {
     </Form>
   );
 }
+
+RegisterView.propTypes = {
+  register: PropTypes.shape({
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+    Email: PropTypes.string.isRequired,
+  }),
+  onRegistration: PropTypes.func,
+};
